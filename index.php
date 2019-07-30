0509bdd44da66ffee8a169a246adddf8776c7e1a
Nadav Tasger <tashernadav@gmail.com>
2019-07-30 22:43:55 +0300

    Changes

--- index.php
+++ index.php
@@ -27,27 +27,6 @@
         function get(v) {
             return (typeof "" === typeof v || typeof '' === typeof v) ? document.getElementById(v) : v;
         }
-
-        function cookie_pull(name) {
-            name += "=";
-            const cookies = document.cookie.split(';');
-            for (let i = 0; i < cookies.length; i++) {
-                let cookie = cookies[i];
-                while (cookie.charAt(0) === ' ') {
-                    cookie = cookie.substring(1);
-                }
-                if (cookie.indexOf(name) === 0) {
-                    return decodeURIComponent(cookie.substring(name.length, cookie.length));
-                }
-            }
-            return undefined;
-        }
-
-        function cookie_push(name, value) {
-            const date = new Date();
-            date.setTime(date.getTime() + (365 * 24 * 60 * 60 * 1000));
-            document.cookie = name + "=" + encodeURIComponent(value) + ";expires=" + date.toUTCString() + ";domain=" + window.location.hostname + ";path=/";
-        }
     </script>
 </head>
 <body onload="view('warn')">
